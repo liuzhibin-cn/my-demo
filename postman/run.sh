3ee6df0f4d3c927c51a64f9a04e3f1c15e33a97b
liuzhibin-cn <riccc.cn@gmail.com>
2020-03-16 13:59:57 +0800

    improve postman

--- postman/run.sh
+++ postman/run.sh
@@ -5,7 +5,8 @@ HOME=`pwd`
 
 docker run -d --net=mydemo --name xmysql mydemo/xmysql:0.4.9
 
-NEWMAN_HOME="/Users/richie-home/Documents/workspace/my-demo/postman/newman"
+#NEWMAN_HOME="/Users/richie-home/Documents/workspace/my-demo/postman/newman"
+NEWMAN_HOME="F:\workspace\mydemo\postman\newman"
 docker run --net=mydemo --name newman -v "$NEWMAN_HOME:/etc/newman" -it mydemo/newman:4.6.0 newman run mydemo.postman_collection.json -e docker-env.postman_environment.json --reporters cli,json --reporter-json-export mydemo-report.json
 
 docker stop xmysql
